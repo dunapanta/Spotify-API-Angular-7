765c662061e58049c2f9832c52398210a305adc1
Daniel Unapanta <dunapanta2@gmail.com>
2019-02-01 16:15:06 -0500

    Añadiendo Cambios

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,71 +1,60 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 //importar map reactive extentions
-import { map } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 
 // Por lo general cuando se trabaja con API
 // Es necesario Centralizar la Informacion por eso este Service
 
 // Este servicio se va a poder Inyectar en otros Componentes
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SpotifyService {
-
   constructor(private http: HttpClient) {
-    console.log('Spotify Service Listo');
+    // console.log('Spotify Service Listo');
   }
 
   // Para  consulta generica
-  getQuery( query: string ) {
-
-    const url = `https://api.spotify.com/v1/${ query }`;
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
 
     //Pipe transformacion de Datos
     // Defino Headers que API de Spotify Necesita
     const headers = new HttpHeaders({
       Authorization:
-        "Bearer BQD0udIHYJmEKts0VMQshXmZ6m3ZQW9nwtsukI9krsNGKKBhk3DO9RZ7bII2Hi3RGnccooYyU-EwdxUt6s0"
+        "Bearer BQCItPNevyVExFrIcz2Lu36MiTNW7A-6pmsZDcfD302RoQhiK97POPBWecTOu4B38DARuVmjJvBPZfI-Mvo"
     });
 
     return this.http.get(url, { headers });
-
   }
 
-  //Cuando API Spotify envia la respuesta envia demasiada informacion y MAP 
-  //simplemente me filtra lo que a mi me sirve 
-
- 
+  //Cuando API Spotify envia la respuesta envia demasiada informacion y MAP
+  //simplemente me filtra lo que a mi me sirve
 
   getNewReleases() {
-
-    return this.getQuery('browse/new-releases?limit=20')
-              .pipe( map( data => data['albums'].items ));
-
+    return this.getQuery("browse/new-releases?limit=20").pipe(
+      map(data => data["albums"].items)
+    );
   }
 
   // Referente al Search
 
-  getArtistas( termino: string ) {
-
-    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
-                .pipe( map( data => data['artists'].items));
-
+  getArtistas(termino: string) {
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
+      map(data => data["artists"].items)
+    );
   }
 
-  getArtista( id: string ) {
-
-    return this.getQuery(`artists/${ id }`);
-                // .pipe( map( data => data['artists'].items));
-
+  getArtista(id: string) {
+    return this.getQuery(`artists/${id}`);
+    // .pipe( map( data => data['artists'].items));
   }
 
-  getTopTracks( id: string ) {
-
-    return this.getQuery(`artists/${ id }/top-tracks?country=us`)
-                .pipe( map( data => data['tracks']));
-
+  getTopTracks(id: string) {
+    return this.getQuery(`artists/${id}/top-tracks?country=us`).pipe(
+      map(data => data["tracks"])
+    );
   }
-
 }
